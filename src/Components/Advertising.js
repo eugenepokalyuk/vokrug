6ed43abb2fbb0f033aa5096f15b01df69abfe822
eugenepokalyuk <eugene.pokalyuk@gmail.com>
2022-12-30 02:32:36 +0700

    Update 4 blocks

--- src/Components/Advertising.js
+++ src/Components/Advertising.js
@@ -5,10 +5,10 @@ function Block() {
         <div className='uk-container uk-container-vokrug uk-margin-large-top uk-padding vokrug-bg-green'>
             <div>
                 <div data-uk-grid>
-                    <div className='uk-flex uk-flex-middle uk-flex-center uk-flex-column uk-width-1-2 uk-margin-top uk-align-center uk-text-center'>
-                        <h1 className='vokrug-adv-header'>Поможем сделать ваши отношения счастливее</h1>
-                        <a className="uk-button dark vokrug-button" href="#a">Записаться на бесплатную встречу</a>
-                        <p className='uk-text-meta uk-text-center vokrug-adv-meta'>На встрече психолог расскажет как проходят групповые сессии и уточнит ваш запрос.</p>
+                    <div className='uk-flex uk-flex-middle uk-flex-center uk-flex-column uk-margin-top uk-align-center uk-text-center'>
+                        <h1 className='vokrug-adv-header'>Поможем вам позаботится <br />о себе и своих отношениях</h1>
+                        <a className="uk-button dark vokrug-button vokrug-margin-top-40" href="#a">Записаться на бесплатную встречу</a>
+                        <p className='uk-text-meta uk-text-center vokrug-adv-meta vokrug-margin-top-24'>На встрече психолог расскажет как проходят<br /> групповые сессии и уточнит ваш запрос.</p>
                     </div>
                 </div>
             </div>
--- src/Components/MeatingBlock.js
+++ src/Components/MeatingBlock.js
@@ -2,48 +2,103 @@ import React from 'react';
 
 function Block() {
     return(
-        <div className='uk-container uk-margin-large-top vokrug-width-90'>
-            <h1 className='uk-text-center vokrug-h2-text uk-margin-large-bottom'>Как проходят встречи</h1>
-            
-            <div className="uk-child-width-1-2@s uk-grid-collapse uk-text-center" data-uk-grid>
-                <div className="uk-child-width-1-2@s uk-grid-match uk-container-vokrug" data-uk-grid>
-                    <div>
-                        <div className="uk-card uk-card-secondary uk-card-body">
-                            <h3 className="uk-card-title">Приветствие</h3>
+    <div className='uk-container uk-margin-large-top vokrug-width-90'>
+        <h1 className='uk-text-center vokrug-h2-text uk-margin-large-bottom'>Как проходят встречи</h1>
+        
+        <div className="uk-grid-collapse uk-child-width-1-2@s uk-grid-match" data-uk-grid>
+            <div>
+                <div className="uk-card vokrug-padding-medium uk-card-default uk-container-vokrug vokrug-height-420 vokrug-shadow-off">
+                    <div className="uk-child-width-1-2@s uk-grid-match vokrug-padding-medium uk-height-1-1" data-uk-grid>
+                        <div>
+                            <div className="uk-card">
+                                <h2 className='vokrug-metting-header'>Приветствие</h2>
+                            </div>
                         </div>
-                    </div>
-                    <div>
-                        <div className="uk-card uk-card-secondary uk-card-hover uk-card-body">
-                            <h3 className="uk-card-title">01.</h3>
+                        <div>
+                            <div className="uk-card">
+                                <p className='uk-align-right vokrug-metting-number'>01.</p>
+                            </div>
                         </div>
-                    </div>
-                    <div>
-                        <div className="uk-card uk-card-secondary uk-card-hover uk-card-body">
-                            <p>Участники рассказывают как прошла их неделя и что чувствуют на момент начала встречи.</p>
+                        <div className='vokrug-width-1-80'>
+                            <div className="uk-card uk-flex uk-flex-bottom">
+                                <p className='vokrug-metting-text uk-margin-remove'>Участники рассказывают <br />как прошла их неделя <br />и что чувствуют на момент <br />начала встречи.</p>
+                            </div>
                         </div>
                     </div>
                 </div>
+            </div>
 
-                <div className="uk-child-width-1-2@s uk-grid-match uk-container-vokrug" data-uk-grid>
-                    <div>
-                        <div className="uk-card uk-card-secondary uk-card-body">
-                            <h3 className="uk-card-title">Приветствие</h3>
+            <div>
+                <div className="uk-card vokrug-padding-medium uk-card-default uk-container-vokrug vokrug-height-420 vokrug-margin-left-2px vokrug-shadow-off">
+                    <div className="uk-child-width-1-2@s uk-grid-match vokrug-padding-medium uk-height-1-1 vokrug-shadow-off" data-uk-grid>
+                        <div>
+                            <div className="uk-card">
+                                <h2 className='vokrug-metting-header'>Активная фаза</h2>
+                            </div>
                         </div>
-                    </div>
-                    <div>
-                        <div className="uk-card uk-card-secondary uk-card-hover uk-card-body">
-                            <h3 className="uk-card-title">01.</h3>
+                        <div>
+                            <div className="uk-card">
+                                <p className='uk-align-right vokrug-metting-number'>02.</p>
+                            </div>
                         </div>
-                    </div>
-                    <div>
-                        <div className="uk-card uk-card-secondary uk-card-hover uk-card-body">
-                            <p>Участники рассказывают как прошла их неделя и что чувствуют на момент начала встречи.</p>
+                        <div className='vokrug-width-1-80'>
+                            <div className="uk-card uk-flex uk-flex-bottom">
+                                <p className='vokrug-metting-text uk-margin-remove'>Ведущий группы задает тему <br />встречи и помогает каждому <br />участнику высказаться, поделиться <br />своим опытом и мнением.</p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        );
+
+        <div className="uk-grid-collapse uk-child-width-1-2@s uk-grid-match vokrug-margin-top-2px" data-uk-grid>
+            <div>
+                <div className="uk-card vokrug-padding-medium uk-card-default uk-container-vokrug vokrug-height-420 vokrug-shadow-off">
+                    <div className="uk-child-width-1-2@s uk-grid-match vokrug-padding-medium uk-height-1-1" data-uk-grid>
+                        <div>
+                            <div className="uk-card">
+                                <h2 className='vokrug-metting-header'>Вопросы и практика</h2>
+                            </div>
+                        </div>
+                        <div>
+                            <div className="uk-card">
+                                <p className='uk-align-right vokrug-metting-number'>03.</p>
+                            </div>
+                        </div>
+                        <div className='vokrug-width-1-80'>
+                            <div className="uk-card uk-flex uk-flex-bottom">
+                                <p className='vokrug-metting-text uk-margin-remove'>Участники задают вопросы психологу, делятся жизненным опытом, обсуждают рекомендации и пробуют применить <br />на занятии советы.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <div className="uk-card vokrug-padding-medium uk-card-default uk-container-vokrug vokrug-height-420 vokrug-margin-left-2px vokrug-shadow-off">
+                    <div className="uk-child-width-1-2@s uk-grid-match vokrug-padding-medium uk-height-1-1" data-uk-grid>
+                        <div>
+                            <div className="uk-card">
+                                <h2 className='vokrug-metting-header'>Итоги</h2>
+                            </div>
+                        </div>
+                        <div>
+                            <div className="uk-card">
+                                <p className='uk-align-right vokrug-metting-number'>04.</p>
+                            </div>
+                        </div>
+                        <div className='vokrug-width-1-80'>
+                            <div className="uk-card uk-flex uk-flex-bottom">
+                                <p className='vokrug-metting-text uk-margin-remove'>Каждый участник рассказывает, <br />что полезного вынес, что понравилось, и как он себя чувствует после встречи. <br />А чтобы закрепить тему ведущий задаёт домашнее задание.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    );
 }
 
 export default Block;

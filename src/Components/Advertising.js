73437f41d0fd7da09c8d07082542dce769056966
eugenepokalyuk <eugene.pokalyuk@gmail.com>
2023-01-08 15:26:07 +0700

    Update Advertising.js

--- src/Components/Advertising.js
+++ src/Components/Advertising.js
@@ -12,16 +12,16 @@ function Block() {
                         <div className='uk-flex uk-flex-middle uk-flex-center uk-flex-column uk-margin-top uk-align-center uk-text-center'>
                             <h1 className='vokrug-adv-header'>Поможем вам позаботится <br />о себе и своих отношениях</h1>
                             <a className="dark vokrug-button" style={maxWidth} data-uk-toggle="target: #modal-example">Записаться на бесплатную встречу</a>
-                            <p className='uk-text-meta uk-text-center vokrug-adv-meta vokrug-margin-top-24'>На встрече психолог расскажет как проходят<br /> групповые сессии и уточнит ваш запрос.</p>
+                            <p className='uk-text-meta uk-text-center vokrug-adv-meta vokrug-margin-top-24'>На встрече психолог расскажет как проходят<br />групповые сессии и уточнит ваш запрос.</p>
                         </div>
                     </div>
                 </div>
             </div>
 
             <div className='vokrug-mobile-container uk-hidden@l'>
-                <h1>Поможем вам позаботится о себе и своих отношениях</h1>
+                <h1>Поможем вам позаботится о себе<br />и своих отношениях</h1>
                 <a className="dark vokrug-button uk-align-center uk-text-center" href="button" data-uk-toggle="target: #modal-example">Записаться на бесплатную встречу</a>
-                <p className='uk-text-center uk-margin-remove-top uk-margin-remove-bottom'>На встрече психолог расскажет как проходят групповые сессии и уточнит ваш запрос</p>
+                <p className='uk-text-center uk-margin-remove-top uk-margin-remove-bottom'>На встрече психолог расскажет как проходят<br />групповые сессии и уточнит ваш запрос</p>
             </div>
         </div>
         );
